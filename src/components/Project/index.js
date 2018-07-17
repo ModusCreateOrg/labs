1537fe489d58a7e8d346c78d96f61f0b833f4585
Grgur Grisogono <grgur@grgur.com>
2018-07-17 11:27:58 +0200

    alternating project display

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DetailsBtn from '../ProjectDetailButton';
+import image from '../../images/evade-graphic.png';
+import s from './styles.module.scss';
 
 const Project = ({ project }) => (
-  <article>
-    <h2>{project.name}</h2>
-    <h3>{project.headline}</h3>
-    <p>{project.description}</p>
-    <DetailsBtn text="View on GitHub" />
+  <article className={s.project}>
+    <aside className={s.image}>
+      <img src={image} />
+    </aside>
+    <div className={s.content}>
+      <h2>{project.name}</h2>
+      <h3>{project.headline}</h3>
+      <p>{project.description}</p>
+      <DetailsBtn text="View on GitHub" />
+    </div>
   </article>
 );
 
