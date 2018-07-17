e9eebdb57dcc47b2ea57b514fbcdf9ff141e8d24
Grgur Grisogono <grgur@grgur.com>
2018-07-17 11:00:48 +0200

    View on GitHub button

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DetailsBtn from '../ProjectDetailButton';
 
 const Project = ({ project }) => (
   <article>
     <h2>{project.name}</h2>
     <h3>{project.headline}</h3>
     <p>{project.description}</p>
+    <DetailsBtn text="View on GitHub" />
   </article>
 );
 
---
+++ src/components/ProjectDetailButton/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { Link } from 'gatsby';
+import arrow from '../../images/arrow.svg';
+
+import s from './styles.module.scss';
+
+const ProjectDetailButton = ({ text, className, href, ...rest }) => (
+  <Link className={classnames(s.btn, className)} href={href} {...rest}>
+    {text}
+    <img src={arrow} className={s.arrow} alt={text} />
+  </Link>
+);
+
+ProjectDetailButton.propTypes = {
+  href: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+ProjectDetailButton.defaultProps = {
+  className: undefined,
+};
+
+export default ProjectDetailButton;
