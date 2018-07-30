f7e335e5fb98b587928637ea596bfafddb57fdb7
Grgur Grisogono <grgur@grgur.com>
2018-07-30 21:23:40 +0200

    no double backslash

--- src/components/project/Featured/index.js
+++ src/components/project/Featured/index.js
@@ -17,7 +17,7 @@ const Image = ({ filename, ...rest }) => {
       placeholder={
         <img
           height={200}
-          src={`${cloudinaryUrlPrefix}/q_30,w_10/labs/${webpFilename}`}
+          src={`${cloudinaryUrlPrefix}q_30,w_10/labs/${webpFilename}`}
           alt="Modus Labs"
           {...rest}
         />
@@ -26,24 +26,24 @@ const Image = ({ filename, ...rest }) => {
       <picture>
         <source
           media="(min-width: 64rem)"
-          srcSet={`${cloudinaryUrlPrefix}/q_70,w_180/labs/${webpFilename} 180w,
-            ${cloudinaryUrlPrefix}/q_70,w_360/labs/${webpFilename} 2x`}
+          srcSet={`${cloudinaryUrlPrefix}q_70,w_180/labs/${webpFilename} 180w,
+            ${cloudinaryUrlPrefix}q_70,w_360/labs/${webpFilename} 2x`}
           sizes="200px"
           type="image/webp"
         />
 
         <source
           media="(min-width: 64rem)"
-          srcSet={`${cloudinaryUrlPrefix}/q_70,w_180/labs/${filename} 180w,
-            ${cloudinaryUrlPrefix}/q_70,w_360/labs/${filename} 2x`}
+          srcSet={`${cloudinaryUrlPrefix}q_70,w_180/labs/${filename} 180w,
+            ${cloudinaryUrlPrefix}q_70,w_360/labs/${filename} 2x`}
           sizes="200px"
           type="image/png"
         />
 
         <img
-          srcSet={`${cloudinaryUrlPrefix}/f_auto,q_70,w_200/labs/${filename} 200w,
-            ${cloudinaryUrlPrefix}/f_auto,q_70,w_400/labs/${filename} 2x`}
-          src={`${cloudinaryUrlPrefix}/f_auto,q_70,w_200/labs/${filename}`}
+          srcSet={`${cloudinaryUrlPrefix}f_auto,q_70,w_200/labs/${filename} 200w,
+            ${cloudinaryUrlPrefix}f_auto,q_70,w_400/labs/${filename} 2x`}
+          src={`${cloudinaryUrlPrefix}f_auto,q_70,w_200/labs/${filename}`}
           sizes="200px"
           alt="Modus Labs"
           {...rest}
