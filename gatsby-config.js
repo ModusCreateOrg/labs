08516689d9a4a3875b2dd367a5fd69b987aa5139
Grgur Grisogono <grgur@grgur.com>
2018-07-31 09:06:05 +0200

    Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,17 @@ module.exports = {
         path: './src/data/',
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Modus Create Labs',
+        short_name: 'Modus Labs',
+        start_url: '/',
+        background_color: '#1B1B1B',
+        theme_color: '#DF6F68',
+        display: 'minimal-ui',
+        icon: 'src/images/modus-black.png',
+      },
+    },
   ],
 };
