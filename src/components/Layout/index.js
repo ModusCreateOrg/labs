a3427233f52ebad2d1ac933454fe0ff75247d947
Grgur Grisogono <grgur@grgur.com>
2018-08-23 13:30:40 +0200

    Updated meta tags

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -55,16 +55,33 @@ export default class Layout extends React.Component {
               nowebp: webpSupported === false,
             })}
           >
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Modus Create Labs' },
-                {
-                  name: 'keywords',
-                  content: 'Modus Create Labs, React, React Native, Vue, Angular, AWS, Devops',
-                },
-              ]}
-            />
+            <Helmet>
+              <title>Modus Labs by Modus Create</title>
+              <meta
+                property="description"
+                content="Modus Create Labs. Code examples that make world's most exciting applications. Developed by Modus Create's certified industry experts. React, Vue, Ionic, Angular, NodeJS, Amazon Web Services, AWS, Cloudformation, Terraform, Serverless, Lambda, Mobile native, iOS, Android"
+              />
+              <meta property="keywords" content="Modus Labs by Modus Create" />
+              <meta property="og:title" content="Modus Labs by Modus Create" />
+              <meta
+                property="og:description"
+                content="Offering tour packages for individuals or groups."
+              />
+              <meta
+                property="og:image"
+                content="https://res.cloudinary.com/modus-labs/image/upload/v1531492623/labs/logo-black.svg"
+              />
+              <meta property="og:url" content="https://labs.moduscreate.com" />
+              <meta
+                name="twitter:card"
+                content="https://res.cloudinary.com/modus-labs/image/upload/v1531492623/labs/logo-black.svg"
+              />
+
+              <meta property="og:site_name" content="Modus Labs" />
+              <meta name="twitter:image:alt" content="Modus Labs" />
+
+              <meta name="twitter:site" content="@moduscreate" />
+            </Helmet>
             <Header siteTitle={data.site.siteMetadata.title} />
             <main className={s.main}>
               <div className={s.innerContent}>{children}</div>
