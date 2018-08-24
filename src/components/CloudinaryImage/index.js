510aa9c77988777559a8cb0f02ffda178d5bee28
Grgur Grisogono <grgur@grgur.com>
2018-08-24 15:34:17 +0200

    Removed incorrectly used Cloudinary secureDistribution

--- src/components/CloudinaryImage/index.js
+++ src/components/CloudinaryImage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Image } from 'cloudinary-react';
 
 const CloudinaryImage = ({ id, ...rest }) => (
-  <Image secureDistribution cloudName="modus-labs" publicId={`labs/${id}`} {...rest} />
+  <Image cloudName="modus-labs" publicId={`labs/${id}`} {...rest} />
 );
 
 export default CloudinaryImage;
