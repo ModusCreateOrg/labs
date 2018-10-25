f5d0757a90a88b3c7366e8ec80de64c96f52db43
Nikhil Kumar <nikhil.kumar@moduscreate.com>
2018-10-25 14:44:06 +0530

    Created team section

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -15,6 +15,13 @@ const GithubButton = ({ text, className, href, ...rest }) => (
   </a>
 );
 
+const TwitterButton = ({ text, className, href, ...rest }) => (
+  <a href={href} {...rest}>
+    <CloudinaryImage id="twitter" filename="twitter.svg" filters={[]} className={s.twitter_icon} alt="twitter" />
+  </a>
+);
+
+
 const DetailsPage = ({ pageContext: { project } }) => {
   return (
     <Layout>
@@ -30,29 +37,48 @@ const DetailsPage = ({ pageContext: { project } }) => {
           </aside>
         </article>
 
-        <article className={s.overview} className={s.content_container}>
-          <div className={s.overview_content} className={s.main}>
+        <article className={classnames(s.overview, s.content_container)}>
+          <div className={classnames(s.overview_content, s.main)}>
             <h3 className={s.title_props}>Project Overview</h3>
             <div className={s.project_description}>{project.description}</div>
           </div>
-          <div className={s.data} className={s.side}>
+          <div className={classnames(s.data, s.side)}>
             <div className={s.stack}>
               <h2 className={s.sub_title}>Technology</h2>
               {project.stack.map((stacks, index) => {
                 return <span key={`teck_stacks_${index}`} className={s.technology_content}> {(index ? ', ' : '') + stacks} </span>
               })
-            }
+              }
             </div>
             <div className={s.tags_section}>
               <h2 className={s.sub_title}>Tags</h2>
-              {project.tags.map(tag => (
-                <div className={s.tags_parent}>
-                  <div className={s.tags_child}>
-                    {tag}
-                  </div>
+              {project.tags.map((tag, index) => (
+                <div className={s.tags_parent} key={`tag_index${index}`}>
+                  <div className={s.tags_child}>{tag}</div>
                 </div>
               ))}
             </div>
+          </div>
+        </article>
+        <article className={s.other_content_container}>
+          <h2 className={classnames(s.team_heading)}>Team</h2>
+          <div className={classnames(s.team)}>
+            {project.team.map((t, index) => {
+              return (
+                <div className={classnames(s.member)} key={`member_name${index}`}>
+                  <img className={s.team_avatar} src={`https://avatars.io/twitter/` + t.twitter} />
+                  <div className={s.member_details}>
+                    <span className={classnames(s.title_props, s.member_name)}>{t.name}</span>
+                    <br /><a href={`https://twitter.com/` + t.twitter}>
+                      <span className={classnames(s.twitter_button)}>
+                        {/* @{t.twitter} <img src={require('../images/twitter-temp.png')} alt="t" className={s.twitter_icon} /> */}
+                        @{t.twitter}      <TwitterButton text={t.twitter} />
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </article>
       </section>
