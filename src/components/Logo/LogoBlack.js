b6b19356335371a5c08ebbfda2165ec54eee74ea
Alex Serb <alexandru.serb@sciencelogic.com>
2019-02-01 23:26:22 +0200

    28 addressed PR comments

--- src/components/Logo/LogoBlack.js
+++ src/components/Logo/LogoBlack.js
@@ -6,7 +6,7 @@ import s from './styles.module.scss';
 
 const LogoBlack = ({ className }) => (
   <div className={classnames(s.logoContainerBlack, className)}>
-    <AnimatedLogo s={s} classnames={classnames} />
+    <AnimatedLogo />
   </div>
 );
 
