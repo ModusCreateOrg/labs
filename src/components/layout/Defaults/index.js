5a560eca1e38df4ee128b67688f6dfa11de9d019
Grgur Grisogono <grgur@grgur.com>
2020-10-21 16:30:10 +0200

    changed the seo image to cloudinary

--- src/components/layout/Defaults/index.js
+++ src/components/layout/Defaults/index.js
@@ -68,12 +68,12 @@ export default class HomeLayout extends React.Component {
               />
               <meta
                 property="og:image"
-                content="https://moduscreate.com/wp-content/uploads/2020/10/Modus_Labs_SEO_Image.png"
+                content="https://res.cloudinary.com/modus-labs/image/upload/v1603290559/labs/Modus_Labs_SEO_Image.png"
               />
               <meta property="og:url" content="https://labs.moduscreate.com" />
               <meta
                 property="twitter:card"
-                content="https://moduscreate.com/wp-content/uploads/2020/10/Modus_Labs_SEO_Image.png"
+                content="https://res.cloudinary.com/modus-labs/image/upload/v1603290559/labs/Modus_Labs_SEO_Image.png"
               />
 
               <meta property="og:site_name" content="Modus Labs" />
