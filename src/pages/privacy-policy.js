b645c2e1930bdc5808708fa7c5e0cd914dd656db
Cristopher Ocaña <crissoca@gmail.com>
2021-08-27 11:14:36 -0500

    Update src/pages/privacy-policy.js

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -5,7 +5,7 @@ import s from '../components/layout/ProjectLandingPage/styles.module.scss';
 const PrivacyPage = () => (
   <main className={s.main}>
     <div className={s.innerContent}>
-      <h3> Cookie List from Modus Labs</h3>
+      <h3>Cookie List from Modus Labs</h3>
       <article><div id="ot-sdk-cookie-policy" /></article>
     </div>
   </main>
