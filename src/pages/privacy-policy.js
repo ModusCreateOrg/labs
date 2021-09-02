23811364f955b485f89caf6d19a807cf7833d88a
Manuel Rodriguez <rodriguez.manu@gmail.com>
2021-09-02 19:49:04 -0300

    testing

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import s from '../components/layout/ProjectLandingPage/styles.module.scss';
 
-const PrivacyPage = () => (
-  <main className={s.main}>
-    <div className={s.innerContent}>
-      <h3>Cookie List from Modus Labs</h3>
-      <article><div id="ot-sdk-cookie-policy" /></article>
-    </div>
-  </main>
-);
+const PrivacyPage = () => {
+  React.useEffect(() => {
+    console.log(window);
+  }, []);
+
+  return (
+    <main className={s.main}>
+      <div className={s.innerContent}>
+        <h3>Cookie List from Modus Labs</h3>
+        <article><div id="ot-sdk-cookie-policy" /></article>
+      </div>
+    </main>
+  );
+};
 
 export default PrivacyPage;
