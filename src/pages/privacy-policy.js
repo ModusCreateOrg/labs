ef063811105e9b644c70d143f0c5684f5c2eaaf9
Manuel Rodriguez <manuel.rodriguez@moduscreate.com>
2021-09-03 20:03:36 -0300

    Iframe updates (#107)
    * Image onetrust
    
    * Adding updates create iframe moduscreate site
    
    * Update src/pages/privacy-policy.js
    
    * testing frame
    
    * Adding iframe options
    
    * updates iframe
    
    * testing
    
    * testing optanon
    
    * testing
    
    * testing
    
    * testing
    
    * revert iframe options
    
    * pr suggested changes
    
    * testing
    
    Co-authored-by: Cristopher Ocaña <crissoca@gmail.com>

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 import s from '../components/layout/ProjectLandingPage/styles.module.scss';
 
-const PrivacyPage = () => (
-  <main className={s.main}>
-    <div className={s.innerContent}>
-      <h3>Cookie List from Modus Labs</h3>
-      <article><div id="ot-sdk-cookie-policy" /></article>
-    </div>
-  </main>
-);
+const PrivacyPage = () => {
+  React.useEffect(() => {
+    const timer = setTimeout(() => window.Optanon.Close(), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <main className={s.main}>
+      <div className={s.innerContent}>
+        <h3>Cookie List from Modus Labs</h3>
+        <article><div id="ot-sdk-cookie-policy" /></article>
+      </div>
+    </main>
+  );
+};
 
 export default PrivacyPage;
